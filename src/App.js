ca5e9eb732941c1b89cc8f87b7df824181a7894a
DanaONeil <doneil@gmail.com>
2020-03-31 15:52:59 -0400

    setting up workflow for frontend with React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+export const App = () => {
+  return (
+   <div className="App">
+     <header className="header">
+       <h1>Project Masters</h1>
+     </header>
+   </div>
+  );
+}
+
+export default App;
